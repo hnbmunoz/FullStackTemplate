35b5d71d66dd6581d7096c89fbb6e659f99b4ab3
Henri Nicholli Muñoz <hnbmunoz@gmail.com>
2022-12-22 10:50:30 +0800

    With Sample Zustand Implementation

--- frontend/react/src/pages/Login/Login.jsx
+++ frontend/react/src/pages/Login/Login.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
+import userProfileStore from '../../store/zustandStore/useProfileStore.jsx'
 
 const Login = () => {
   const [userName, setUserName] = useState('')
   const [userPassword, setUserPassword] = useState('')
+
+  const { profile, overwriteProfile, clearProfile } = userProfileStore(
+    (state) => ({
+      profile: state.profile,
+      overwriteProfile: state.overwriteProfile,
+      clearProfile: state.clearProfile,
+    })
+  );
 
   const handleUserName = (e) => {
     setUserName(e.currentTarget.value)
@@ -24,6 +33,7 @@ const Login = () => {
       }
     }).then(res => {
       // setInputMessage("");
+      overwriteProfile(res.headers)
       console.log(res)
       // console.log(res.data)
     }).catch(ex => {
